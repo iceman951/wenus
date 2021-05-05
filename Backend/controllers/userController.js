a30b5eab1a05b0dbb4c1839bba8cafea355829c3
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-05-05 17:33:13 +0700

    add get users

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,27 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
 
+exports.show = async (req, res, next) => {
+    try {
+        const user = await User.find().select('-password');
+        if (!user) {
+            throw new Error('ไม่พบข้อมูลผู้ใช่');
+        }
+
+        res.status(200).json({
+            message: 'สำเร็จ',
+            data: user
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            error: {
+                message: 'เกิดผิดพลาด ' + error.message
+            } 
+        });
+    }
+}
+
 exports.register = async (req, res, next) => {
     try {
         const { firstName, lastName, email, password, birthDate, faculty } = req.body;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -4,9 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.show);
 
 /* http://localhost:3000/users/register */
 router.post('/register', [
