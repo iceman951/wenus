c79e02e2e4a0013bc6d6d59fd2e2cdeed4f4a261
pasitto <6010110478@psu.ac.th>
2021-05-11 22:07:42 +0700

    create views

--- frontend/hubApp/src/routes/index.js
+++ frontend/hubApp/src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from '../components/ProductList'
+import Home from '../views/Home'
 
 const Routing = () => {
     return(
