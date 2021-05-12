9dab80f220591bbea5aad4bd8c9e100d83e58ac0
pasitto <6010110478@psu.ac.th>
2021-05-12 09:38:21 +0700

    add AppBar & PostsInputBox

--- frontend/hubApp/src/views/Home/index.js
+++ frontend/hubApp/src/views/Home/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import AppBar from "./AppBar";
+import PostsInput from "./PostsInput";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome Home Page</h1>
-        </div>
-    )
-}
+  // Get Token
+  const storedJwt = localStorage.getItem("token");
+  const [jwt, setJwt] = useState(storedJwt || null);
+  
 
-export default Home
+  return (
+    <div
+      style={{
+        backgroundColor: "#424242",
+        height: window.innerHeight,
+      }}
+    >
+      <AppBar />
+      <h1>Welcome Home Page</h1>
+      <PostsInput token={jwt}/>
+    </div>
+  );
+};
+
+export default Home;
