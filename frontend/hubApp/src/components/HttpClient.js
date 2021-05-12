5de335e299dd38567ea287b08da31a0c777d0a25
keng <keng@new>
2021-05-12 11:27:55 +0700

    review login&registerPage

--- frontend/hubApp/src/components/HttpClient.js
+++ frontend/hubApp/src/components/HttpClient.js
@@ -4,10 +4,10 @@ import join from 'url-join'
 axios.interceptors.request.use(async (config) => {
     const jwtToken = await localStorage.getItem("token")
     if (jwtToken != null){
-        config.headers = {'x-access-token': jwtToken}
+        config.headers = {'Authorization': `Bearer ${jwtToken}`}
     }
     config.url = join('https://guarded-falls-57008.herokuapp.com/', config.url);
     return config;
 });
 
-export const httpClient = axios
+export const Axios = axios
--- frontend/hubApp/src/components/RegisterForm.js
+++ frontend/hubApp/src/components/RegisterForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
-import { httpClient } from "./HttpClient";
+import { Axios } from "./HttpClient";
 
 import {
   Grid,
@@ -46,7 +46,7 @@ const RegisterForm = () => {
     validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
-      httpClient.post(`/users/register`, values).then((res) => {
+      Axios.post(`/users/register`, values).then((res) => {
         console.log(res.data);
         console.log(res);
       });
@@ -57,110 +57,132 @@ const RegisterForm = () => {
       <Grid container>
         <Grid item xs={12} sm={6} style={{ marginBottom: "3%" }}>
           <Grid container justify="center">
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="firstName">firstName</InputLabel>
-              <OutlinedInput
-                id="firstName"
-                label="firstName"
-                type="text"
-                onChange={formik.handleChange}
-                defaultValue={formik.values.firstName}
-              />
-            </FormControl>
-            {formik.errors.firstName ? (
-              <div>{formik.errors.firstName}</div>
-            ) : null}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="firstName">firstName</InputLabel>
+                <OutlinedInput
+                  id="firstName"
+                  label="firstName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  defaultValue={formik.values.firstName}
+                />
+              </FormControl>
+              {formik.errors.firstName ? (
+                <div>{formik.errors.firstName}</div>
+              ) : null}
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12} sm={6} style={{ marginBottom: "3%" }}>
           <Grid container justify="center">
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="lastName">lastName</InputLabel>
-              <OutlinedInput
-                id="lastName"
-                label="lastName"
-                type="text"
-                onChange={formik.handleChange}
-                defaultValue={formik.values.lastName}
-              />
-            </FormControl>
-            {formik.errors.lastName ? (
-              <div>{formik.errors.lastName}</div>
-            ) : null}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="lastName">lastName</InputLabel>
+                <OutlinedInput
+                  id="lastName"
+                  label="lastName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  defaultValue={formik.values.lastName}
+                />
+              </FormControl>
+              {formik.errors.lastName ? (
+                <div>{formik.errors.lastName}</div>
+              ) : null}
+            </Grid>
           </Grid>
         </Grid>
 
         <Grid item xs={12} sm={6} style={{ marginBottom: "3%" }}>
           <Grid container justify="center">
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="email">Email Address</InputLabel>
-              <OutlinedInput
-                id="email"
-                label="Email Address"
-                type="email"
-                onChange={formik.handleChange}
-                defaultValue={formik.values.email}
-              />
-            </FormControl>
-            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="email">Email Address</InputLabel>
+                <OutlinedInput
+                  id="email"
+                  label="Email Address"
+                  type="email"
+                  onChange={formik.handleChange}
+                  defaultValue={formik.values.email}
+                />
+              </FormControl>
+              {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12} sm={6} style={{ marginBottom: "3%" }}>
           <Grid container justify="center">
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <OutlinedInput
-                id="password"
-                label="Password"
-                type="password"
-                onChange={formik.handleChange}
-                defaultValue={formik.values.password}
-              />
-            </FormControl>
-            {formik.errors.password ? (
-              <div>{formik.errors.password}</div>
-            ) : null}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <OutlinedInput
+                  id="password"
+                  label="Password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  defaultValue={formik.values.password}
+                />
+              </FormControl>
+              {formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+              ) : null}
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12} sm={6} style={{ marginBottom: "3%" }}>
           <Grid container justify="center">
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="faculty">Faculty</InputLabel>
-              <OutlinedInput
-                id="faculty"
-                label="Faculty"
-                type="text"
-                onChange={formik.handleChange}
-                defaultValue={formik.values.faculty}
-              />
-            </FormControl>
-            {formik.errors.faculty ? <div>{formik.errors.faculty}</div> : null}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="faculty">Faculty</InputLabel>
+                <OutlinedInput
+                  id="faculty"
+                  label="Faculty"
+                  type="text"
+                  onChange={formik.handleChange}
+                  defaultValue={formik.values.faculty}
+                />
+              </FormControl>
+              {formik.errors.faculty ? (
+                <div>{formik.errors.faculty}</div>
+              ) : null}
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12} sm={6} style={{ marginBottom: "3%" }}>
           <Grid container justify="center">
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="birthdate" shrink>
-                Birthdate
-              </InputLabel>
-              <OutlinedInput
-                id="birthdate"
-                label="Birthdate"
-                type="date"
-                onChange={formik.handleChange}
-                defaultValue={formik.values.birthdate}
-              />
-            </FormControl>
-            {formik.errors.birthdate ? (
-              <div>{formik.errors.birthdate}</div>
-            ) : null}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="birthdate" shrink>
+                  Birthdate
+                </InputLabel>
+                <OutlinedInput
+                  id="birthdate"
+                  label="Birthdate"
+                  type="date"
+                  onChange={formik.handleChange}
+                  defaultValue={formik.values.birthdate}
+                />
+              </FormControl>
+              {formik.errors.birthdate ? (
+                <div>{formik.errors.birthdate}</div>
+              ) : null}
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12} style={{ marginBottom: "1%" }}>
           <Grid container justify="center">
-            <Button variant="contained" type="submit" color="primary">
-              Register
-            </Button>
+            <Grid item xs={6}>
+              <Button
+                size="large"
+                fullWidth
+                variant="contained"
+                type="submit"
+                color="primary"
+              >
+                Register
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
