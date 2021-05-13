1eb8732ccf116cef8ce35be6d21d59a0ba547e27
pasitto <6010110478@psu.ac.th>
2021-05-13 09:50:09 +0700

    del productlist

--- frontend/hubApp/src/views/Home/index.js
+++ frontend/hubApp/src/views/Home/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import AppBar from "./AppBar";
-import CreatePost from "./CreatePost";
 // import Posts from "../../components/Posts"
 
 const Home = () => {
@@ -13,8 +12,6 @@ const Home = () => {
       }}
     >
       <AppBar />
-      {/* <h1>Welcome Home Page</h1> */}
-      {/* <PostsInput token={jwt}/> */}
       <CreatePost />
     </div>
   );
