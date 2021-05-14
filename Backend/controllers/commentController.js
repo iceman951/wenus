4007a790cde1e3c8d376863458ba501662954fac
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-05-14 15:35:14 +0700

    db change to embedded

--- Backend/controllers/commentController.js
+++ Backend/controllers/commentController.js
@@ -3,15 +3,21 @@ const Post = require("../models/post");
 
 exports.create = async (req, res, next) => {
   try {
-    const { id, text } = req.body;
+    const { post_id, text } = req.body;
+    let post = await Post.findById({ _id: post_id });
+
+    if (!post) {
+      const error = new Error("ไม่พบข้อมูลโพสต์ที่ต้องการเพิ่มคอมเมนต์");
+      error.statusCode = 404;
+      throw error;
+    }
+
     let comment = new Comment({
       text: text,
       author: req.user._id,
     });
 
-    let post = await Post.findById({ _id: id });
-    await comment.save();
-    await post.comments.push({ _id: comment._id });
+    await post.comments.push(comment);
     await post.save();
 
     res.status(201).json({
@@ -25,12 +31,18 @@ exports.create = async (req, res, next) => {
 
 exports.delete = async (req, res, next) => {
   try {
-    const { post_id, comment_id } = req.body;
-    await Comment.deleteOne({ _id: comment_id });
-    await Post.updateOne(
-      { _id: post_id },
-      { $pullAll: { comments: [comment_id] } }
-    );
+    const { comment_id } = req.body;
+
+    let post = await Post.findOne({comments: {$elemMatch: {_id: comment_id}}});
+
+    if (!post) {
+      const error = new Error("ไม่พบข้อมูลคอมเมนต์ที่ต้องการลบ");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    await post.comments.pull({_id: comment_id});
+    await post.save();
 
     res.status(200).json({
       success: true,
@@ -43,15 +55,19 @@ exports.delete = async (req, res, next) => {
 
 exports.edit = async (req, res, next) => {
   try {
-    const { id, text } = req.body;
-    const comment = await Comment.updateOne(
-      { _id: id },
-      {
-        text: text,
-      }
-    );
+    const { comment_id, text } = req.body;
+    let post = await Post.findOne({comments: {$elemMatch: {_id: comment_id}}});
 
-    if (comment.nModified === 0) {
+    if (!post) {
+      const error = new Error("ไม่พบข้อมูลคอมเมนต์ที่ต้องการแก้ไข");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    console.log(post);
+    // await post.save();
+
+    if (post.nModified === 0) {
       throw new Error("ไม่สามารถอัปเดตข้อมูลได้");
     } else {
       res.status(200).json({
@@ -59,6 +75,21 @@ exports.edit = async (req, res, next) => {
         message: "แก้ไขข้อมูลเรียบร้อย",
       });
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.show = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const posts = await Post.find({comments: {$elemMatch: {_id: id}}});
+
+    res.status(200).json({
+      success: true,
+      message: "สำเร็จ",
+      data: posts,
+    });
   } catch (error) {
     next(error);
   }
