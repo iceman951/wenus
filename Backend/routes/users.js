48de93e27f83f6ea75e70dce039b6ab704d9f206
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-05-18 10:09:01 +0700

    add get profile

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,24 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { body } = require('express-validator');
-const userController = require('../controllers/userController');
-const passportJWT = require('../middleware/passportJWT');
+const { body } = require("express-validator");
+const userController = require("../controllers/userController");
+const passportJWT = require("../middleware/passportJWT");
 
 /* GET users listing. */
-router.get('/', [passportJWT.isLogin], userController.show);
+router.get("/", [passportJWT.isLogin], userController.show);
+
+/* http://localhost:3000/users/me */
+router.get("/me", [passportJWT.isLogin], userController.me);
 
 /* http://localhost:3000/users/login */
-router.post('/login', userController.login);
+router.post("/login", userController.login);
 
 /* http://localhost:3000/users/register */
-router.post('/register', [
-  body('firstName').not().isEmpty().withMessage('กรุณากรอกชื่อ'),
-  body('lastName').not().isEmpty().withMessage('กรุณากรอกนามสกุล'),
-  body('email').not().isEmpty().withMessage('กรุณากรอกอีเมล์').isEmail().withMessage('รูปแบบอีเมล์ไม่ถูกต้อง'),
-  body('password').not().isEmpty().withMessage('กรุณากรอกรหัสผ่านด้วย').isLength({min: 3}).withMessage('รหัสผ่านต้อง 3 ตัวอักษรขึ้นไป'),
-  body('birthdate').not().isEmpty().withMessage('กรุณากรอกวันเกิด').isDate().withMessage('รูปแบบวันที่ไม่ถูกต้อง'),
-  body('faculty').not().isEmpty().withMessage('กรุณากรอกคณะ')
-
-] ,userController.register);
+router.post(
+  "/register",
+  [
+    body("firstName").not().isEmpty().withMessage("กรุณากรอกชื่อ"),
+    body("lastName").not().isEmpty().withMessage("กรุณากรอกนามสกุล"),
+    body("email")
+      .not()
+      .isEmpty()
+      .withMessage("กรุณากรอกอีเมล์")
+      .isEmail()
+      .withMessage("รูปแบบอีเมล์ไม่ถูกต้อง"),
+    body("password")
+      .not()
+      .isEmpty()
+      .withMessage("กรุณากรอกรหัสผ่านด้วย")
+      .isLength({ min: 3 })
+      .withMessage("รหัสผ่านต้อง 3 ตัวอักษรขึ้นไป"),
+    body("birthdate")
+      .not()
+      .isEmpty()
+      .withMessage("กรุณากรอกวันเกิด")
+      .isDate()
+      .withMessage("รูปแบบวันที่ไม่ถูกต้อง"),
+    body("faculty").not().isEmpty().withMessage("กรุณากรอกคณะ"),
+  ],
+  userController.register
+);
 
 module.exports = router;
