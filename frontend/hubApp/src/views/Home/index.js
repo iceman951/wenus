2c22eff6a10350d6dce4b9eb940480eb3a2f430a
keng <keng@new>
2021-05-25 12:49:33 +0700

    add bg image

--- frontend/hubApp/src/views/Home/index.js
+++ frontend/hubApp/src/views/Home/index.js
@@ -6,18 +6,23 @@ import TagsBar from "./TagsBar";
 import { useSelector } from "react-redux";
 import Login from "../LoginPage/index";
 
+const WINDOW_HEIGHT = window.innerHeight;
 const Home = () => {
   const loggedIn = useSelector((state) => state.user.loggedIn);
   if (!loggedIn) {
-    return <Login />
+    return <Login />;
   }
   return (
-    <>
+    <div
+      style={{
+        backgroundImage: `url(/assets/images/bg1.jpg)`
+      }}
+    >
       <NavBar />
       <TagsBar />
       <CreatePost />
       <Posts />
-    </>
+    </div>
   );
 };
 
