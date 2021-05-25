4931d5b6c40f66fcb5957fa4c4cd616b1d49ccae
keng <keng@new>
2021-05-25 11:52:31 +0700

    tag bug fix

--- frontend/hubApp/src/views/Home/Posts.js
+++ frontend/hubApp/src/views/Home/Posts.js
@@ -4,7 +4,7 @@ import Post from "./Post";
 import { Container } from "@material-ui/core/";
 import { getMyPost, getPosts } from "../../store/actions/postAction";
 import { useDispatch, useSelector } from "react-redux";
-import Loading from './Loading'
+import Loading from "./Loading";
 
 const WINDOW_HEIGHT_50 = window.innerHeight / 2;
 
@@ -14,21 +14,24 @@ export default function Posts() {
   const posts = useSelector((state) => state.post.posts);
   const selectedTag = useSelector((state) => state.tag.selectedTag);
   const isLoading = useSelector((state) => state.post.loading);
-  const dbPostsLength = useSelector((state) => state.post.dbPostsLength)
+  const dbPostsLength = useSelector((state) => state.post.dbPostsLength);
   const [skip, setSkip] = useState(0);
 
   //Load post when selectedTag changed
   useEffect(() => {
     dispatch({ type: "RESET_POST" });
     setSkip(0);
+    if (selectedTag === "โพสต์ของฉัน") getMyPost(dispatch, 0);
+    else getPosts(dispatch, selectedTag, 0, 0);
   }, [dispatch, selectedTag]);
 
   //Load next posts
   useEffect(() => {
     // console.log(skip);
-    if (selectedTag === "โพสต์ของฉัน") getMyPost(dispatch, skip);
-    else getPosts(dispatch, selectedTag, skip, dbPostsLength);
-  }, [dispatch, selectedTag, skip]);
+    if (skip !== 0) {
+      getPosts(dispatch, selectedTag, skip, dbPostsLength);
+    }
+  }, [dispatch, skip]);
 
   //scroll
   useEffect(() => {
@@ -38,7 +41,7 @@ export default function Posts() {
       // console.log(clientHeight, scrollTop, scrollHeight, e);
 
       if (clientHeight + scrollTop + WINDOW_HEIGHT_50 >= scrollHeight) {
-        setSkip(skip+5);
+        setSkip(skip + 5);
         // console.log("---------", clientHeight, scrollTop, scrollHeight)
       }
     };
@@ -57,9 +60,7 @@ export default function Posts() {
           <Post key={post._id} post={post} />
         </LazyLoad>
       ))}
-      {
-      isLoading ? <Loading />: <></>
-      }
+      {isLoading ? <Loading /> : <></>}
     </Container>
   );
 }
